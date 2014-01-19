f5f807141362616b82551f8766e80452b2dadc5b
Lance Stacenko <stacenko.lance@yahoo.ca>
2014-01-19 16:56:53 -0500

    Cherry-Picking dev to master

--- bitcointicker.py
+++ bitcointicker.py
@@ -15,8 +15,12 @@ lcd.begin(16,1)
 def BTC():
 	mtgox = json.load(urllib2.urlopen('http://data.mtgox.com/api/1/BTCUSD/ticker'))
 	if mtgox["result"] != "success":
+<<<<<<< HEAD
 		lcd.message("MtGox API call failed! :(")
 		raise Exception("MtGox API call failed!")
+=======
+		raise Exception("MtGox API failed!")
+>>>>>>> e60d8dd... Added exception handling in main loop, tested OK
  
 	current_price = mtgox["return"]["last"]["value"]
 
@@ -30,7 +34,15 @@ def BTC():
 while 1:
 	lcd.clear()
 	#Calls BTC function, gets time and formats.
+<<<<<<< HEAD
 	price = BTC()
+=======
+	try:
+		price = BTC()
+	except Exception:
+		lcd.message("MtGox API failed! :(")
+
+>>>>>>> e60d8dd... Added exception handling in main loop, tested OK
 	time = datetime.now().strftime( '%F %H:%M\n' )
 	#Displays time on first line, BTC/USD rate on next line.
 	lcd.message(time)
