e60d8ddb02991b7056df4ae6cdb8263555a2ab85
Lance Stacenko <stacenko.lance@yahoo.ca>
2014-01-19 16:56:53 -0500

    Added exception handling in main loop, tested OK

--- bitcointicker.py
+++ bitcointicker.py
@@ -15,7 +15,7 @@ lcd.begin(16,1)
 def BTC():
 	mtgox = json.load(urllib2.urlopen('http://data.mtgox.com/api/1/BTCUSD/ticker'))
 	if mtgox["result"] != "success":
-		raise Exception("MtGox API call failed!")
+		raise Exception("MtGox API failed!")
  
 	current_price = mtgox["return"]["last"]["value"]
 
@@ -39,17 +39,10 @@ def DOGE():
 while 1:
 	lcd.clear()
 	#Calls BTC function, gets time and formats.
-
-	#
-	# Handle all query exceptions in the main loop
-	# because the possibility of generating other types
-	# of exceptions for various things exists
-	# so you can generate output based on that. - Raf
-	#
 	try:
 		price = BTC()
 	except Exception:
-		lcd.message("MtGox API call failed! :(")
+		lcd.message("MtGox API failed! :(")
 
 	time = datetime.now().strftime( '%F %H:%M\n' )
 	#Displays time on first line, BTC/USD rate on next line
